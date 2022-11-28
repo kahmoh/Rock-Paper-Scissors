3a010fad7f6675770d4709ed710a14170aeac4a5
kahmoh <joshmcfadden17@gmail.com>
2022-11-28 17:54:25 +0000

    add function to get player choice with a prompt

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 //pick for computer
 //let the player pick
 //return the player choice
+//function to get player and computer choice at once
 //determine the winner of a given round
 //tell the player who won the given round
 //keep score of round wins
 //first to five wins
 //button to reset game
 
+
 const CHOICES = ['rock','paper','scissors'];
+let winner
 
-
-function getComputerChoice() {
-    return CHOICES [Math.floor(Math.random() * CHOICES.length)]
+function getPlayerChoice(){
+    let input = prompt('Type Rock, paper or scissors')
+    input = input.toLowerCase()
 }
 
-console.log(getComputerChoice())
+function getComputerChoice() {
+    return CHOICES [Math.floor(Math.random() * 3 )]
+}
+
+
