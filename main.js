6a09770bfc8f3a031989fa861e4329a3015f5225
kahmoh <joshmcfadden17@gmail.com>
2023-02-12 18:50:16 +0000

    rework function to get player choice

--- main.js
+++ main.js
@@ -1,76 +1,16 @@
-const CHOICES = ['rock', 'paper', 'scissors']
-const winners = []
-let playerWins = 0;
-let computerWins = 0;
-let roundCount = 0;
+const BUTTONS = document.querySelectorAll('button')
 
-function playGame() {
-  while (playerWins < 3 && computerWins <3){
-    playRound()
+const CHOICES = ['rock', 'paper', 'scissors'];
+
+function getPlayerChoice () {
+  for (let button of BUTTONS) {
+    button.addEventListener('click', (e) => {
+      const playerPick = button.textContent
+      console.log(playerPick)
+    })
   }
-  if (playerWins == 3) {
-    console.log('GAME OVER! You win :)')
-  }else if (computerWins == 3) {
-    console.log('GAME OVER! You lose :(')
-  }
-}
+};
 
-function playRound() {
-  const playerChoice = getPlayerChoice();
-  const computerChoice = getComputerChoice();
-  declareWinner(playerChoice, computerChoice);
-  const winner = winners[winners.length-1]
-  logRound(playerChoice, computerChoice, winner)
-}
+getPlayerChoice()
 
-function getPlayerChoice() {
-  let input = prompt('Type Rock, paper or scissors');
-  while (input === null){
-    input = prompt('Type Rock, Paper or scissors')
-  }
-  input = input.toLowerCase();
-  return input;
-}
 
-function getComputerChoice() {
-  return CHOICES[Math.floor(Math.random() * 3)];
-}
-
-function declareWinner(choiceP, choiceC) {
-  if (choiceC == choiceP) {
-    winners.push('Tie');
-    roundCount ++
-  } else if (
-      (choiceP == 'rock' && choiceC == 'scissors') ||
-      (choiceP == 'paper' && choiceC == 'rock') ||
-      (choiceP == 'scissors' && choiceC == 'paper')
-  ) {
-    winners.push('You')
-    roundCount ++
-    playerWins ++
-  } else if (
-      (choiceC == 'rock' && choiceP == 'scissors') ||
-      (choiceC == 'paper' && choiceP == 'rock') ||
-      (choiceC == 'scissors' && choiceP == 'paper')
-  ) {
-    winners.push('Computer')
-    roundCount ++
-    computerWins ++
-  }
-}
-
-function logRound (playerChoice, computerChoice, winner) {
-  const playerChoiceCaps = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
-  const computerChoiceCaps = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
-  console.log('Round:', roundCount);
-  console.log('You chose:', playerChoiceCaps);
-  console.log('Computer chose:', computerChoiceCaps);
-  if (winner !== 'Tie'){
-    console.log(winner, 'won that round')
-  }else {
-    console.log('That was a tie')
-  }
-  console.log('=================================')
-}
-
-playGame()
