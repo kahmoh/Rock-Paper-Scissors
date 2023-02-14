4d17c4a262fedbf865e13174d834bae24334886e
kahmoh <joshmcfadden17@gmail.com>
2023-02-14 16:59:25 +0000

    change win messages for round end

--- main.js
+++ main.js
@@ -11,6 +11,41 @@ function getPlayerChoice () {
   }
 };
 
+function getComputerChoice() {
+  console.log(CHOICES[Math.floor(Math.random() * 3)]);
+}
+
+function declareWinner(choiceP, choiceC) {
+  if (choiceC == choiceP) {
+    // winners.push('Tie');
+    // roundCount ++
+    console.log('Tie')
+  } else if (
+      (choiceP == 'rock' && choiceC == 'scissors') ||
+      (choiceP == 'paper' && choiceC == 'rock') ||
+      (choiceP == 'scissors' && choiceC == 'paper')
+  ) {
+    console.log('You win')
+    // winners.push('You')
+    // roundCount ++
+    // playerWins ++
+  } else if (
+      (choiceC == 'rock' && choiceP == 'scissors') ||
+      (choiceC == 'paper' && choiceP == 'rock') ||
+      (choiceC == 'scissors' && choiceP == 'paper')
+  ) {
+    console.log('You lose')
+    // winners.push('Computer')
+    // roundCount ++
+    // computerWins ++
+  }
+}
+
+
 getPlayerChoice()
+
+//add function to get computer choice
+//add function to compare computer choice with player choice and determine a winner of a given round
+//add function to display stats of a given round (player selection, computer selection, winner)
 
 
