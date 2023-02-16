7895c5a982f5cdd3aea971b3f2eb51c9a57ffc33
kahmoh <joshmcfadden17@gmail.com>
2023-02-16 21:11:11 +0000

    add end screen functionality

--- main.js
+++ main.js
@@ -1,29 +1,48 @@
 const BUTTONS = document.querySelectorAll('h1')
+const SCORE = document.querySelector('.score')
+const ENDSCREEN = document.querySelector('.endscreen')
 
 const CHOICES = ['rock', 'paper', 'scissors'];
+let playerWins = 0;
+let computerWins = 0;
+let gameState = 'playing';
 
-function getPlayerChoice (btn) {
-  return(btn.textContent.toLowerCase());
+function restartGame () {
+  playerWins = 0;
+  computerWins = 0;
+  ENDSCREEN.classList.remove('win')
+  ENDSCREEN.classList.remove('loss')
+  ENDSCREEN.textContent = ''
+  SCORE.textContent = (`SCORE: ${playerWins} - ${computerWins}`)
+  gameState = 'playing';
 }
 
-for (let button of BUTTONS) {
-  button.addEventListener('click', () => {
-    const playerChoice = getPlayerChoice(button);
-    const computerChoice = getComputerChoice();
-    console.log(`Player chose ${playerChoice}`)
-    console.log(`Computer chose ${computerChoice}`)
-    declareWinner(playerChoice,computerChoice)
-  })
+function showLossScreen() {
+  const endScreenText = document.createElement('h1')
+  endScreenText.textContent = 'YOU LOSE'
+  ENDSCREEN.appendChild(endScreenText)
+  ENDSCREEN.classList.add('loss')
+  setTimeout(restartGame,1000)
+}
+
+function showWinScreen() {
+  const endScreenText = document.createElement('h1')
+  endScreenText.textContent = 'YOU WIN'
+  ENDSCREEN.appendChild(endScreenText)
+  ENDSCREEN.classList.add('win')
+  setTimeout(restartGame,2000)
+}
+
+function getPlayerChoice(btn) {
+  return (btn.textContent.toLowerCase());
 }
 
 function getComputerChoice() {
-  return(CHOICES[Math.floor(Math.random() * 3)]);
+  return (CHOICES[Math.floor(Math.random() * 3)]);
 }
 
 function declareWinner(choiceP, choiceC) {
   if (choiceC === choiceP) {
-    // winners.push('Tie');
-    // roundCount ++
     console.log('Tie')
   } else if (
       (choiceP === 'rock' && choiceC === 'scissors') ||
@@ -31,24 +50,37 @@ function declareWinner(choiceP, choiceC) {
       (choiceP === 'scissors' && choiceC === 'paper')
   ) {
     console.log('You win')
-    // winners.push('You')
-    // roundCount ++
-    // playerWins ++
+    playerWins++
   } else if (
       (choiceC === 'rock' && choiceP === 'scissors') ||
       (choiceC === 'paper' && choiceP === 'rock') ||
       (choiceC === 'scissors' && choiceP === 'paper')
   ) {
     console.log('You lose')
-    // winners.push('Computer')
-    // roundCount ++
-    // computerWins ++
+    computerWins++
   }
 }
 
+for (let button of BUTTONS) {
+  button.addEventListener('click', () => {
+    if (gameState === 'playing') {
+      playRound(button)
+    }
+  })
+}
 
-//add function to get computer choice
-//add function to compare computer choice with player choice and determine a winner of a given round
-//add function to display stats of a given round (player selection, computer selection, winner)
-
-
+function playRound(BTN) {
+  const playerChoice = getPlayerChoice(BTN);
+  const computerChoice = getComputerChoice();
+  console.log(`Player chose ${playerChoice}`)
+  console.log(`Computer chose ${computerChoice}`)
+  declareWinner(playerChoice, computerChoice)
+  SCORE.textContent = (`SCORE: ${playerWins} - ${computerWins}`)
+  if (playerWins >= 5) {
+    gameState = 'over';
+    showWinScreen()
+  }else if (computerWins >= 5){
+    gameState = 'over';
+    showLossScreen()
+  }
+}
